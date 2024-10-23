542e3237757d3177eca9ade57c7ed312e232c498
Carl Mitchell <griseldacarl@gmail.com>
2024-10-23 13:55:43 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Hello Git
+
+Hello Git is a Python file that displays Hello Git.
+
+## Usage
+
+```python
+
+# prints 'Hello Git'
+print("Hello Git!!")
+
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+None
---
+++ hellogit.py
@@ -0,0 +1,19 @@
+"""
+
+Carl Mitchell
+Module 08 Project 
+CIS256 33773
+Part B
+Create a folder and initialize it as a repo. In that repo, create at least three files:
+
+A Python program/script. The contents don't really matter, it can be something you're working on, code from a previous project, or even just a "Hello World".
+A file called README.md. A GitHub convention is to include readme file with a description of the project/repo, and it's usually in markdownLinks to an 
+external site. format (hence the .md extension). The contents don't matter, but you're welcome to experiment with Markdown syntax to see how it works. 
+VS Code supports Markdown with dynamic previewLinks to an external site.. You could also use something like https://www.makeareadme.com/Links to an external
+site. to throw something together.
+A third file of your choice. Maybe another .py file. Maybe a picture of your cat.
+Commit and publish your repo on GitHub. Then, make changes to at least two of the files and commit them. You should have at least two commits on GitHub.
+    
+"""
+
+print("Hello Git!!")
